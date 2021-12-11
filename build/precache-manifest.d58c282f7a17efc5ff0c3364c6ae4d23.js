e9e4a038c61d44dbd165527258d96f072e2e1fe9
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-12-11 15:12:29 +0700

    initial commit

---
+++ build/precache-manifest.d58c282f7a17efc5ff0c3364c6ae4d23.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-restricted-globals */
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f46d6d0d4e853c0ec0dba52c1dfac1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa536cd9a8a326016b26",
+    "url": "/static/css/main.fb16d91f.chunk.css"
+  },
+  {
+    "revision": "6cf2a3fc067a65147de0",
+    "url": "/static/js/2.ca53fadf.chunk.js"
+  },
+  {
+    "revision": "a64acf8e3ad6aa1497056f0bb9a7ce4f",
+    "url": "/static/js/2.ca53fadf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa536cd9a8a326016b26",
+    "url": "/static/js/main.ae49bc9a.chunk.js"
+  },
+  {
+    "revision": "221d158a633b14d4161e",
+    "url": "/static/js/runtime-main.0edb2c7f.js"
+  }
+]);
